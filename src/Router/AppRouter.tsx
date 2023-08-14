8649cf51ea8f0d0a2881bfd38281f81865362173
Rodrigo Grande <96559980+rodrigogz64@users.noreply.github.com>
2023-08-14 03:16:58 -0600

    Update AppRouter.tsx

--- src/Router/AppRouter.tsx
+++ src/Router/AppRouter.tsx
@@ -1,8 +1,6 @@
 import {Routes, Route} from "react-router-dom";
 import Mainnet from "../views/Mainnet";
 import Testnet from "../views/Testnet";
-/* import Liquid from "../views/Liquid";
-import Liquidtestnet from "../views/Liquidtestnet"; */
 
 export const AppRouter = () => {
     return (
@@ -10,8 +8,6 @@ export const AppRouter = () => {
             <Route path="/login" element={<h1>Hola mundo</h1>}/>
             <Route path="/" element={<Mainnet/>}/>
             <Route path="/Testnet" element={<Testnet/>}/>
-           {/*  <Route path="/Liquid" element={<Liquid/>}/>
-            <Route path="/Liquidtestnet" element={<Liquidtestnet/>}/> */}
             
             <Route path="/*" element={ <h1>404</h1> }/>
         </Routes>
