ea31f50aa5728ec4a0936956c7ee67ad6e943d09
Rodrigo Grande <96559980+rodrigogz64@users.noreply.github.com>
2024-04-21 20:15:37 -0600

    Update

--- src/Router/AppRouter.tsx
+++ src/Router/AppRouter.tsx
@@ -3,7 +3,6 @@ import SplashScreen from "../components/Splash/SplashScreen";
 import Mainnet from "../views/Mainnet";
 import Testnet from "../views/Testnet";
 import Signet from "../views/Signet";
-import NotFound from "../components/NotFound/NotFound";
 
 export default function AppRouter() {
   return (
@@ -14,8 +13,7 @@ export default function AppRouter() {
       <Route path="/Testnet" element={<Testnet />} />
       <Route path="/Signet" element={<Signet />} />
 
-      <Route path="/*" element={<NotFound />} />
+      <Route path="/*" element={<h1>404</h1>} />
     </Routes>
   );
 }
-
